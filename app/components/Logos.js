8b3e41bb3349d0529778e2580dd18827d3a7ce13
Stasa Bastrica <stasabastrica7@gmail.com>
2025-02-22 17:29:22 +0100

    Initial commit

--- app/components/Logos.js
+++ app/components/Logos.js
@@ -25,20 +25,18 @@ const Logos = () => {
       direction="left"
       gradient={true}
       gradientColor={"var(--light-blue)"}
-      gradientWidth={300}
-      pauseOnHover={false}
+      gradientWidth={200}
+      pauseOnHover={true}
       play={true}
       delay={0}
       loop={0}
       autoFill={true}
-      pauseOnHover={true}
     >
       {logos.map((src, index) => (
         <Image
           key={index}
           src={src}
           alt={`Image ${index + 1}`}
-          style={{ margin: "0 90px" }}
           width={150}
           height={100}
           className={styles.logo}
